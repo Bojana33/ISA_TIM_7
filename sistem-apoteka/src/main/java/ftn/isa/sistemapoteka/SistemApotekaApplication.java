4778e815152a34649445c9555b7673530e7e17d1
borissrbotnjak <borissrbotnjak@gmail.com>
2020-11-09 20:30:23 +0100

    init
    inicijalni push

---
+++ sistem-apoteka/src/main/java/ftn/isa/sistemapoteka/SistemApotekaApplication.java
@@ -0,0 +1,13 @@
+package ftn.isa.sistemapoteka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemApotekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemApotekaApplication.class, args);
+	}
+
+}
