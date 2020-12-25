f8758d36b602c7cba3440fe801e7210d4d2ef240
pc <miroslav9gag@gmail.com>
2020-12-25 22:26:17 +0100

    Dodate potrebne klase. Nepopunjene naravno

--- sistem-apoteka/src/main/java/ftn/isa/sistemapoteka/Entity/Pharmacy.java
+++ sistem-apoteka/src/main/java/ftn/isa/sistemapoteka/Entity/Pharmacy.java
@@ -1,4 +1,16 @@
 package ftn.isa.sistemapoteka.Entity;
 
+import java.util.List;
+
 public class Pharmacy {
+    private Integer id;
+    private String name;
+    private String address;
+    private String description;
+    private List<Appointment> freeDermatologistAppointments;
+    private List<Dermatolgist> dermatologists;
+    private List<Pharmacist> pharmacists;
+    private List<Drug> drugs;
+
+
 }
