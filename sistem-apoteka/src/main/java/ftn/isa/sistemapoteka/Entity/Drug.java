7f1ac569f53da8140f1252244c19066c9cf01f13
Bojana33 <bojana.todorovic12@gmail.com>
2021-01-30 12:45:45 +0100

    Popunjavanje klasa Drug i Patient

--- sistem-apoteka/src/main/java/ftn/isa/sistemapoteka/Entity/Drug.java
+++ sistem-apoteka/src/main/java/ftn/isa/sistemapoteka/Entity/Drug.java
@@ -1,4 +1,44 @@
 package ftn.isa.sistemapoteka.Entity;
 
+import javax.persistence.*;
+import javax.swing.text.StyledEditorKit;
+
+@Entity
 public class Drug {
+
+    @Id
+    private Long code;
+
+    @Column
+    private String name;
+
+    @Column
+    private String contraindications;
+
+    @Column
+    private String structure;
+
+    @Column
+    private int dailyIntake;
+
+    @Column
+    private Boolean reserved;
+
+    @Column
+    private String producer;
+
+    @Column
+    private Boolean onPrescription;
+
+    @Column
+    private String additionalNote;
+
+    @Column
+    private float loyaltyPoints;
+
+    @Column
+    private int quantity;
+
+    @Column
+    private float price;
 }
--- sistem-apoteka/src/main/java/ftn/isa/sistemapoteka/Entity/Patient.java
+++ sistem-apoteka/src/main/java/ftn/isa/sistemapoteka/Entity/Patient.java
@@ -5,13 +5,38 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name = "patient")
 public class Patient extends RegisteredUser{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@Column(name = "id")
     private Long id;
 
-    @OneToMany(mappedBy = "patient")
+    @Column
+    private Float loyaltyPoints;
+
+    @OneToMany(targetEntity = Drug.class)
+    private Set<Drug> allergies = new HashSet<>();
+
+    @OneToMany(targetEntity = Appointment.class)
     private Set<Appointment> appointments = new HashSet<>();
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Set<Appointment> getAppointments() {
+        return appointments;
+    }
+
+    public void setAppointments(Set<Appointment> appointments) {
+        this.appointments = appointments;
+    }
+
 }
