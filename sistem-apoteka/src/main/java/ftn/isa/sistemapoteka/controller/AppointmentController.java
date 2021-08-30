b438df1314d756367dcb312c07a03724c9c91ebf
Dijana <dijana.mukic@synechron.com>
2021-08-30 20:40:58 +0200

    Calendar for dermatologist.

--- sistem-apoteka/src/main/java/ftn/isa/sistemapoteka/controller/AppointmentController.java
+++ sistem-apoteka/src/main/java/ftn/isa/sistemapoteka/controller/AppointmentController.java
@@ -2,8 +2,11 @@ package ftn.isa.sistemapoteka.controller;
 
 import ftn.isa.sistemapoteka.service.AppointmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/appointments")
@@ -14,5 +17,11 @@ public class AppointmentController {
     @Autowired
     public AppointmentController(AppointmentService appointmentService){
       this.appointmentService = appointmentService;
+    }
+
+    @PreAuthorize("hasRole('DERMATOLOGIST')")
+    @GetMapping("/dermatologist/calendar")
+    public ModelAndView dermatologistCalendar(){
+        return new ModelAndView("Calendar1");
     }
 }
