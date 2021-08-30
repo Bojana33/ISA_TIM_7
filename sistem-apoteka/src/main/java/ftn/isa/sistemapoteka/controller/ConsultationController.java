9eb0ff1e146f1835f11b83e7fe16a4f0bc7968cf
Dijana <dijana.mukic@synechron.com>
2021-08-30 09:08:59 +0200

    Work on pharmacist home page.

--- sistem-apoteka/src/main/java/ftn/isa/sistemapoteka/controller/ConsultationController.java
+++ sistem-apoteka/src/main/java/ftn/isa/sistemapoteka/controller/ConsultationController.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("consultations")
+@RequestMapping("/consultations")
 public class ConsultationController {
 
     private ConsultationService consultationService;
